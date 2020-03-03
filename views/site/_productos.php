3776a6e4ad1185308a261c38d5ae36ce7dba352e
Oscar Megía López <pfgytk@gmail.com>
2020-03-03 13:30:37 +0100

    Modificado para que centre las imagenes

--- views/site/_productos.php
+++ views/site/_productos.php
@@ -4,7 +4,9 @@ use yii\helpers\Html;
 
 <div class="col-sm-6 col-md-6">
   <div class="thumbnail">
+    <div style="text-align: center; margin-top: 5%;">
     <?= Html::img('@web//imgs/' . $model->foto, ['width' => '400px']) ?>
+    </div>
     <div class="caption">
       <h3><?= $model->id ?></h3>
       <p><?= $model->nombre ?></p>
