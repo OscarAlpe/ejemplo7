9d248f4cefe587722f320353f919760a4623e3e7
Oscar Megía López <pfgytk@gmail.com>
2020-03-03 11:04:09 +0100

    Añadida opción productos y renombrada la vista oferta a productos para que usen la misma vista

--- views/site/productos.php
+++ views/site/productos.php
@@ -4,6 +4,7 @@
 <div class="row">
     <?= ListView::widget([
         'dataProvider' => $dataProvider,
-        'itemView' => '_productos'
+        'itemView' => '_productos',
+        'layout' => '<div>{summary}</div><br /><div class="container container-fluid">{items}</div><div>{pager}</div>',
     ]); ?>
 </div>
