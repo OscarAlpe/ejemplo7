b92b6f8019660b0e96f98196066bb011d8d59a2a
Oscar Megía López <pfgytk@gmail.com>
2020-03-09 11:26:17 +0100

    Añadido para que muestre la foto en el detalle de la categoría

--- views/site/detalle_categoria.php
+++ views/site/detalle_categoria.php
@@ -1,10 +1,24 @@
 <?php
 use yii\widgets\DetailView;
 use yii\grid\GridView;
+use yii\helpers\Html;
 ?>
 
 <?= DetailView::widget([
     'model' => $model,
+    'attributes' => [
+        'id',
+        'nombre',
+        'descripcion',
+        [
+            'label' => 'foto',
+            'format' => 'raw',
+            'value' => Html::img("@web/imgs/" . $model->foto, [
+                'class' => 'fotoDetalle',
+            ]),
+            'contentOptions' => ['style' => 'text-align: center;']
+        ]
+    ]
 ]) ?>
 
 <?= GridView::widget([
